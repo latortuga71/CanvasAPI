aa96b065b8ae5e9e7f3200dd493ec98f3b0f6750
Latortuga <42878263+latortuga71@users.noreply.github.com>
2025-12-28 21:48:04 -0500

    Implement mesh data fetching and rendering
    Added mesh data loading and rendering functionality.

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 console.log("main.js");
 
+
+// Mesh Data
+const MeshVertices = [
+    { x : -1, y : -1, z : -1 }, // 1
+    { x : -1, y :  1, z : -1 }, // 2
+    { x :  1, y :  1, z : -1 }, // 3
+    { x :  1, y : -1, z : -1 }, // 4
+    { x :  1, y :  1, z :  1 }, // 5
+    { x :  1, y : -1, z :  1 }, // 6
+    { x : -1, y :  1, z :  1 }, // 7
+    { x : -1, y : -1, z :  1 }  // 8
+];
+
+const MeshFaces = [
+    { a : 1, b : 2, c : 3 },
+    { a : 1, b : 3, c : 4 },
+    { a : 4, b : 3, c : 5 },
+    { a : 4, b : 5, c : 6 },
+    { a : 6, b : 5, c : 7 },
+    { a : 6, b : 7, c : 8 },
+    { a : 8, b : 7, c : 2 },
+    { a : 8, b : 2, c : 1 },
+    { a : 2, b : 7, c : 5 },
+    { a : 2, b : 5, c : 3 },
+    { a : 6, b : 8, c : 1 },
+    { a : 6, b : 1, c : 4 }
+];
+
+
 // Globals
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -8,6 +37,9 @@ const CanvasWidth = 800;
 const CanvasHeight = 600;
 const FovFactor = 640;
 
+
+
+
 // Pixel in buffer -> width * X + Y = pixel
 //const FrameBuffer = new Uint32Array(CanvasWidth * CanvasHeight);
 
@@ -15,13 +47,26 @@ const FovFactor = 640;
 const CubeVertices = [];
 const CubeVerticesProjected = [];
 const CameraPosition = {x: 0, y: 0 , z: -5 };
+const TrianglesToRender = [];
 
+
+const CubeMeshVertices = [];
+const CubeMeshFaces = [];
 
 function DrawPixel(point,color){
   if (point.x >= 0 && point.x < CanvasWidth && point.y >= 0 && point.y < CanvasHeight){
     ctx.fillStyle = color;
     ctx.fillRect(point.x,point.y,1,1);
   }
+}
+function DrawLine(x1,y1,x2,y2,color){
+  const path = new Path2D();
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 1;
+  ctx.lineCap = "butt"; // butt  round  square <-- other options
+  path.moveTo(x1, y1);
+  path.lineTo(x2, y2);
+  ctx.stroke(path);
 }
 
 function ClearCanvas() {
@@ -37,6 +82,11 @@ function DrawGrid() {
   }
 }
 
+function DrawTriangle(xy1,xy2,xy3,color){
+  DrawLine(xy1.x,xy1.y,xy2.x,xy2.y,color);
+  DrawLine(xy2.x,xy2.y,xy3.x,xy3.y,color);
+  DrawLine(xy3.x,xy3.y,xy1.x,xy1.y,color);
+}
 
 function DrawRect(x,y,w,h,color){
   for (let i = 0; i < w; i++){
@@ -48,7 +98,19 @@ function DrawRect(x,y,w,h,color){
   }
 }
 
-function Init(){
+async function FetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return response.text();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function Init(){
   let i = 0;
   for (let x = -1; x <= 1; x+= 0.25){
     for (let y = -1; y <= 1; y+= 0.25){
@@ -59,8 +121,37 @@ function Init(){
       }
     }
   }
-  // Setup GameLoop 
-  //setTimeout(GameLoop,1000/FPS);
+  // Get The MeshData From File
+  const cubeMeshData = await FetchData("./assets/cube.obj")
+  //const cubeMeshData = await FetchData("./assets/f22.obj")
+  console.log(cubeMeshData);
+  // Parse the data
+  let lines = cubeMeshData.split("\n");
+  for (let i = 0; i < lines.length; i++){
+
+    if (lines[i].startsWith("v")) {
+      let line_split = lines[i].split(" ");
+      let vec3 = {
+        x : parseFloat(line_split[1]),
+        y : parseFloat(line_split[2]),
+        z : parseFloat(line_split[3])
+      }
+      CubeMeshVertices.push(vec3);
+    }
+    // vertex texture normal
+    //f 1/1/1 2/2/1 3/3/1
+    if (lines[i].startsWith("f")) {
+      let line_split = lines[i].split(" ");
+      let a = parseInt(line_split[1].split("/")[0]);
+      let b = parseInt(line_split[2].split("/")[0]);
+      let c = parseInt(line_split[3].split("/")[0]);
+
+      CubeMeshFaces.push({a:a,b:b,c:c});
+
+    }
+  }
+  console.log(CubeMeshFaces);
+  console.log(CubeMeshVertices);
 }
 
 /* --- Transforms  --- */
@@ -99,22 +190,84 @@ let _cube_rotation_y = 0.0;
 let _cube_rotation_x = 0.0;
 let _cube_rotation_z = 0.0;
 
-function Update(delta) {
-  _cube_rotation_y  += 0.01;
+  // Old Way to render cube
+  /*
   for (let i = 0; i < (9*9*9); i++){
     let _p = CubeVertices[i];
     let transformed_p = RotateVec3Y(_p, _cube_rotation_y);
     transformed_p.z -= CameraPosition.z;
     CubeVerticesProjected[i] = ProjectPoint(transformed_p);
   }
+*/
+
+function Update(delta) {
+  _cube_rotation_y  += 0.01;
+  for (let i = 0; i < CubeMeshFaces.length; i++){
+    // Get Face Vertices
+    const face = CubeMeshFaces[i];
+    let faceVertices = [];
+    faceVertices[0] = CubeMeshVertices[face.a - 1]
+    faceVertices[1] = CubeMeshVertices[face.b - 1]
+    faceVertices[2] = CubeMeshVertices[face.c - 1]
+    // Transform Each Vertice Of The Face
+    let projected_triangle = [];
+
+    for (let j = 0; j < 3; j++){
+      let transformed_vertex = faceVertices[j];
+      transformed_vertex = RotateVec3Y(transformed_vertex, _cube_rotation_y);
+      transformed_vertex.z -= CameraPosition.z;
+      //CubeVerticesProjected[i] = ProjectPoint(transformed_vertex);
+      // Save the projected point in a triangles to render global
+      let projected_vertex = ProjectPoint(transformed_vertex);
+      projected_vertex.x += (CanvasWidth / 2);
+      projected_vertex.y += (CanvasHeight / 2);
+      projected_triangle[j] = projected_vertex;
+    }
+    TrianglesToRender[i] = projected_triangle;
+  }
+  /*
+  for (let i = 0; i < MeshFaces.length; i++){
+    // Get Face Vertices
+    const face = MeshFaces[i];
+    let faceVertices = [];
+    faceVertices[0] = MeshVertices[face.a - 1]
+    faceVertices[1] = MeshVertices[face.b - 1]
+    faceVertices[2] = MeshVertices[face.c - 1]
+    // Transform Each Vertice Of The Face
+    let projected_triangle = [];
+
+    for (let j = 0; j < 3; j++){
+      let transformed_vertex = faceVertices[j];
+      transformed_vertex = RotateVec3Y(transformed_vertex, _cube_rotation_y);
+      transformed_vertex.z -= CameraPosition.z;
+      //CubeVerticesProjected[i] = ProjectPoint(transformed_vertex);
+      // Save the projected point in a triangles to render global
+      let projected_vertex = ProjectPoint(transformed_vertex);
+      projected_vertex.x += (CanvasWidth / 2);
+      projected_vertex.y += (CanvasHeight / 2);
+      projected_triangle[j] = projected_vertex;
+    }
+    TrianglesToRender[i] = projected_triangle;
+  }
+  */
+
+
 }
 
 function Render(){
   ClearCanvas();
-  //DrawGrid();
-  for (let i = 0; i < (9*9*9); i++){
-    const p = CubeVerticesProjected[i];
-    DrawRect(p.x + (CanvasWidth / 2), p.y + (CanvasHeight / 2), 4, 4, "#FFFF00");
+  for (let i = 0; i < CubeMeshFaces.length; i++){
+    const triangle = TrianglesToRender[i];
+    DrawRect(triangle[0].x, triangle[0].y ,3, 3, "#FFFF00");
+    DrawRect(triangle[1].x, triangle[1].y ,3, 3, "#FFFF00");
+    DrawRect(triangle[2].x, triangle[2].y ,3, 3, "#FFFF00");
+
+    DrawTriangle(
+      triangle[0],
+      triangle[1],
+      triangle[2],
+      "#FFFF00"
+    );
   }
 }
 
